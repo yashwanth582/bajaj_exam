7b06a3435ab2d07ed70add5784e0005aa50771e1
yashwanth582 <94818yashwanth@gmail.com>
2023-09-08 17:01:41 +0530

    no changes now

--- app.js
+++ app.js
@@ -15,30 +15,43 @@ const userData = {
 };
 
 // POST endpoint
+ // POST endpoint
 app.post('/bfhl', (req, res) => {
-    const { data } = req.body;
-  
-    if (!data || !Array.isArray(data)) {
-      return res.status(400).json({ error: 'Invalid request body' });
+  const { data } = req.body;
+
+  if (!data || !Array.isArray(data)) {
+    return res.status(400).json({ error: 'Invalid request body' });
+  }
+
+  // Extract alphabets and find the highest alphabet
+  const alphabets = [];
+  let highest_alphabet = null;
+  const numbers = [];
+
+  for (const char of data) {
+    if (/^[A-Za-z]$/.test(char)) {
+      alphabets.push(char);
+      if (!highest_alphabet || char > highest_alphabet) {
+        highest_alphabet = char;
+      }
+    } else if (/^\d+$/.test(char)) {
+      numbers.push(char);
     }
-  
-    // Extract alphabets and find the highest alphabet
-    const alphabets = data.filter((char) => /^[A-Za-z]$/.test(char));
-    const highest_alphabet = alphabets.sort((a, b) => b.localeCompare(a, undefined, { sensitivity: 'base' }))[0];
-  
-    const response = {
-      is_success: true,
-      user_id: `yashwanthks${userData.dob}`,
-      email: userData.email,
-      roll_number: userData.roll_number,
-      numbers: data.filter((char) => /^\d$/.test(char)),
-      alphabets,
-      highest_alphabet: [highest_alphabet],
-    };
-  
-    res.json(response);
-  });
-  
+  }
+
+  const response = {
+    is_success: true,
+    user_id: `yashwanth_ks_${userData.dob}`,
+    email: userData.email,
+    roll_number: userData.roll_number,
+    numbers,
+    alphabets,
+    highest_alphabet: highest_alphabet ? [highest_alphabet] : [],
+  };
+
+  res.json(response);
+});
+
 
 // GET endpoint
 app.get('/bfhl', (req, res) => {
